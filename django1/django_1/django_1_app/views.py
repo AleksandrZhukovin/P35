d9653e082d3174ebb3f1b534da5a7a6efdb06ae1
AleksandrZhukovin <sashokzuk@gmail.com>
2024-01-11 21:51:47 +0200

    add(templates): add base.html. post request on home

--- django1/django_1/django_1_app/views.py
+++ django1/django_1/django_1_app/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 
 def home(request):
-    name = 'Bob'
-    # context = {'var': name}
-    return render(request, 'home.html', {'var': name})
+    if request.method == 'POST':
+        data = request.POST
+        print(data['text'])
+        name = 'Bob'
+        return redirect('/page2')
+    elif request.method == 'GET':
+        name = 'Bob'
+        # context = {'var': name}
+        return render(request, 'home.html', {'var': name, 'l': [1, 2, 3, 4]})
+
+
+def page2(request):
+    return HttpResponse('Hello')
